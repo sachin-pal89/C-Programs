38d16a1b1e68a8a2f412cd9c8ba4f9fdafc5574d
Sachin Pal <sachinpal01012003@gmail.com>
2021-09-07 13:11:11 +0530

    Created a directory for PCPF Learning purpose

---
+++ PCPF Programs/Inheritance.cpp
@@ -0,0 +1,23 @@
+
+// Roll_No: 37, Name: Sachin Pal
+
+#include<iostream>
+using namespace std;
+
+class Account{
+	public: 
+		float salary = 60000;
+};
+
+class Programmer : public Account {
+	public: 
+		float bonus = 5000;
+};
+
+int main(void){
+	Programmer p1;
+	cout<<"Salary: "<<p1.salary<<endl;
+	cout<<"Bonus: "<<p1.bonus<<endl;
+	return 0;
+}
+
